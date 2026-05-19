55f2685a1f3df46dfc9e698435080732a01d4cc1
Nicholasnishi <ayuminishimura6@gmail.com>
2026-05-19 12:12:23 -0300

    quase pronto

--- inss.js
+++ inss.js
@@ -1,4 +1,4 @@
-const salarioBruto = 3800
+const salarioBruto = 10000
 let inss = 0
 
 if(salarioBruto <= 1621){
@@ -7,6 +7,10 @@ if(salarioBruto <= 1621){
     inss = (salarioBruto - 1621)*0.09 + 121.58
 }else if(salarioBruto > 2902.84 && salarioBruto <= 4354.27){
   inss = ( salarioBruto - 2902.84)*0.12 + 115.37 + 121.58
+}else if(salarioBruto > 4354.27 && salarioBruto <= 8475.55){
+    inss = (salarioBruto - 4354.27)*0.14 + 115.37 + 121.58 + 174.17
+}else{
+    inss = 988.10
 }
 
 console.log(inss.toFixed(2))
